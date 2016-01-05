50e4af95cf1999da5a2bda5a7fe8d4ea010564dd
betulmasal <betul.yldrm1205@gmail.com>
2016-01-05 10:49:05 +0200

    frekmdosyasi

--- frek.m
+++ frek.m
@@ -1,25 +1,19 @@
-  function [frekans]=frek(nota,oktav)                                         %Girilen nota ve oktava göre frekans döndürülüyor. 
-notalar={'Do','C#','Re','Eb','Mi','Fa','F#','Sol','G#','La','Bb','Si'};     %Notalarýmýz bir dizi içerisinde yazýldý. 
- n=strcmp(notalar,nota);                                                     %Fonksiyona gönderdiðimiz notayý dizi içerisinde aratýr ve buduðu indekse 1 atar 
- m=find(n,1);                                                                %n dizisinde 1 deðerinin indeksini döndürür. 
- 
- 
- 
- 
- if (nargin==0) 
-       disp ('Lütfen veri giriþi yapýnýz: ') 
-   else if (nargin==1) 
-         if(m==13) 
-             frekans=0; 
-        else 
-           frekans=floor(16.35*(2^4)*(2^((m-1)/12)));                       % Frekans fonksiyonu oluþturuldu.  
-            
-         end 
-     else  
-         if (m==13)  
-            frekans=0; 
-         else frekans=floor(16.35*(2^oktav)*(2^((m-1)/12)));                 % Frekans fonksiyonu oluþturuldu. 
-        end 
-         
-     end 
- end                                                                         %sayýyý en yakýn deðere yuvarladýk yuvarlama kodu. 
+function[frek] = frek(nota,oktav)
+    
+
+    if nargin < 2  % oktav belirtilmemisse,
+        oktav = 4; % 4.oktav kabul ediliyor.
+    end
+    
+    notalar = {'do','dod','re','mib','mi','fa','fad','sol','sold','la','sib','si','sus'};
+    
+    frek = 0; %Sus geldigi zaman geri dönmesi için yazildi
+    
+    index = find(ismember(notalar,nota)); %%istenilen notanin dizideki yeri bulunuyor
+    
+    if index~=13 %%Sus geldiyse hesaplanmayacak
+        base = 440; %%La notasinin 4. oktavdaki degeri
+        frek=base*2^((index-10)/12 + (oktav-4));
+    end
+end
+    
