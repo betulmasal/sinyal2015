1ac77527e97eb88d234ca73c3efddcac09c4f0ac
betulmasal <betul.yldrm1205@gmail.com>
2016-01-05 10:48:47 +0200

    notemdosyasi

--- note.m
+++ note.m
@@ -1,35 +1,38 @@
-function [a,b] = note(f,vurus) %fonksiyonu tanimliyoruz.girilicek argumanlari tanýmladýk
+function [xx,tt] = note(frek,dur)
+    
+    %% "fs" periyot baþýna istenilen örnek sayýsýný verir
 
-fs=8192;%örnekleme frekansý
-
-b = 0:1/fs:vurus; %zaman aralýgý
-
-
-
-
+    fs=8192;
+    
+    T = 1/fs;
+    
+    tt = 0:T:(dur-T); %% Zaman 
+    
+    %%zarf için hazýrlýklar
     elemanS = length(tt); %% Eleman sayýsý
-
     
-
     attack = linspace(0,1.5,(elemanS*2/8));
-
     
-
     decay = linspace(1.5-(0.5/(elemanS/8)),1,(elemanS/8));
-
     
-
     sustain = ones(1,(elemanS/2));
-
     
-
     relase = linspace(1-(1/(elemanS/8)),0,(elemanS/8));
-
     
-
     zarf = [attack, decay, sustain, relase];
 
+     h_k = [1, 0.8, 0.4, 0.1];
 
+     xx = zeros(1,elemanS);
 
+     
+     for i=1:length(h_k)
+        x = h_k(i).* sin(2*pi*i*frek*tt);
 
-    x = zarf .* sin(2*pi*f*t + zarf); %% zarflý sinüs
+        xx = xx + x;
+
+     end
+     
+     xx = xx.*zarf;
+
+    
